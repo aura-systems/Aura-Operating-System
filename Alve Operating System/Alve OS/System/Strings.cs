45854db2be29c66f9509e754cf194c170fd496ac
Alexy <alexy.da-cruz@live.fr>
2017-07-20 17:38:15 +0200

    [WIP] Language

---
+++ Alve Operating System/Alve OS/System/Strings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alve_OS.System
+{
+    class Strings
+    {
+
+        public static void Display(string lang, string ToTranslate)
+        {
+            switch (lang)
+            {
+                case "fr_FR":
+
+                    switch (ToTranslate)
+                    {
+                        case "shutdown":
+                            Console.WriteLine("Extinction en cours...");
+                            break;
+                    }
+
+                    break;
+
+                case "en_US":
+
+                    switch (ToTranslate)
+                    {
+                        case "shutdown":
+                            Console.WriteLine("Shutting Down...");
+                            break;
+                    }
+
+                    break;
+            }
+        }
+
+    }
+}
