1248f3f90f71f71fbb1f8d79c7f0643e0d2b0b73
Alexy <alexy.da-cruz@live.fr>
2017-09-04 21:05:34 +0200

    [wip] New Tips Impl

---
+++ Alve Operating System/Alve OS/System/Computer/Tips.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alve_OS.System.Computer
+{
+    class Tips
+    {
+
+        public static void Switch()
+        {
+        }
+
+        public static void Disable()
+        {
+
+        }
+
+        public static void Enable()
+        {
+
+        }
+
+    }
+}
