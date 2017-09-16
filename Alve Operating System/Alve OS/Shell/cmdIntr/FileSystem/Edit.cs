fe00b10d7d6fa0e1a91235bc39498f39388efa77
John <djlw78@gmail.com>
2017-09-16 10:56:19 -0500

    Added in file's

---
+++ Alve Operating System/Alve OS/Shell/cmdIntr/FileSystem/Edit.cs
@@ -0,0 +1,51 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Command Interpreter - Edit
+* PROGRAMMER(S):    John Welsh <djlw78@gmail.com>
+*/
+
+using System.IO;
+using L = Aura_OS.System.Translation;
+
+namespace Aura_OS.Shell.cmdIntr.FileSystem
+{
+    class Edit
+    {
+        private static string HelpInfo = "";
+
+        /// <summary>
+        /// Getter and Setters for Help Info.
+        /// </summary>
+        public static string HI
+        {
+            get { return HelpInfo; }
+            set { HelpInfo = value; /*PUSHED OUT VALUE (in)*/}
+        }
+
+        /// <summary>
+        /// Empty constructor. (Good for debug)
+        /// </summary>
+        public Edit() { }
+
+        /// <summary>
+        /// c = command, c_Edit
+        /// </summary>
+        /// <param name="edit">The file you wish to edit</param>
+        /// /// <param name="startIndex">The start index for remove.</param>
+        /// <param name="count">The count index for remove.</param>
+        public static void c_Edit(string edit, short startIndex = 0, short count = 5)
+        {
+            string file = edit.Remove(startIndex, count);
+            if (File.Exists(Kernel.current_directory + file))
+            {
+                Apps.User.Editor application = new Apps.User.Editor();
+                application.Start(file, Kernel.current_directory);
+            }
+            else
+            {
+                L.Text.Display("doesnotexit");
+            }
+        }
+
+    }
+}
---
+++ Alve Operating System/Alve OS/Shell/cmdIntr/Settings/Settings.cs
@@ -0,0 +1,117 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Command Interpreter - Settings
+* PROGRAMMER(S):    John Welsh <djlw78@gmail.com>
+*/
+
+using System;
+using System.IO;
+using L = Aura_OS.System.Translation;
+using Aura_OS.System.Users;
+using Aura_OS.System.Computer;
+
+namespace Aura_OS.Shell.cmdIntr.Settings
+{
+    class Settings
+    {
+        private static string HelpInfo = "";
+
+        /// <summary>
+        /// Getter and Setters for Help Info.
+        /// </summary>
+        public static string HI
+        {
+            get { return HelpInfo; }
+            set { HelpInfo = value; /*PUSHED OUT VALUE (in)*/}
+        }
+
+        /// <summary>
+        /// Empty constructor. (Good for debug)
+        /// </summary>
+        public Settings() { }
+
+        /// <summary>
+        /// c = command, c_Settings
+        /// </summary>
+        public static void c_Settings()
+        {
+            L.Help.Settings();
+        }
+
+        public static void c_Settings(string settings)
+        {
+            Char separator = ' ';
+            string[] cmdargs = settings.Split(separator);
+
+            if (cmdargs[1].Equals("adduser"))
+            {
+                //todo remake this method
+                //with users.Create();
+
+                //method user
+                string argsuser = cmdargs[2];
+                Users users = new Users();
+
+                users.Create(argsuser);
+
+            }
+
+            else if (cmdargs[1].Equals("setcomputername"))
+            {
+                //method computername
+                Info.AskComputerName();
+
+            }
+
+            else if (cmdargs[1].Equals("setlang"))
+            {
+                if (cmdargs.Length == 2)
+                {
+                    L.Text.Display("availablelanguage");
+                }
+
+                if ((cmdargs[2].Equals("en_US")) || cmdargs[2].Equals("en-US"))
+                {
+                    Kernel.langSelected = "en_US";
+                    L.Keyboard.Init();
+                    if (File.Exists(@"0:\System\lang.set"))
+                    {
+                        File.WriteAllText(@"0:\System\lang.set", Kernel.langSelected);
+                    }
+                    else
+                    {
+                        File.Create(@"0:\System\lang.set");
+                        File.WriteAllText(@"0:\System\lang.set", Kernel.langSelected);
+                    }
+                }
+                else if ((cmdargs[2].Equals("fr_FR")) || cmdargs[2].Equals("fr-FR"))
+                {
+                    Kernel.langSelected = "fr_FR";
+                    L.Keyboard.Init();
+                    if (File.Exists(@"0:\System\lang.set"))
+                    {
+                        File.WriteAllText(@"0:\System\lang.set", Kernel.langSelected);
+                    }
+                    else
+                    {
+                        File.Create(@"0:\System\lang.set");
+                        File.WriteAllText(@"0:\System\lang.set", Kernel.langSelected);
+                    }
+                }
+                else
+                {
+                    L.Text.Display("unknownlanguage");
+                    L.Text.Display("availablelanguage");
+                }
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                L.Text.Display("UnknownCommand");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+    }
+}
