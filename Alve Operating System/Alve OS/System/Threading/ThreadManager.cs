e140758c9010c704d8657a286e821a1a71ac9cd8
John <djlw78@gmail.com>
2017-09-17 10:42:41 -0500

    A basic example of threading (multitasking)
    Meheheehe!

---
+++ Alve Operating System/Alve OS/System/Threading/ThreadManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.System.Threading
+{
+    public static class ThreadManager
+    {
+        private static List<Thread> threads;
+        private static Queue<ThreadSignal> signals;
+
+        public static void PrepareThreading()
+        {
+            Console.WriteLine("Initializing thread list...");
+            threads = new List<Thread>();
+            Console.WriteLine("Initalizing signal queue...");
+            signals = new Queue<ThreadSignal>();
+        }
+
+        public static void Register(Thread Thrd)
+        {
+            Thrd.TID = threads.Count + 1;
+            //log
+            threads.Add(Thrd);
+        }
+
+        public static void ProcessSignals()
+        {
+            for (int i = 0; i < signals.Count; i++)
+            {
+                ThreadSignal ThrdSgnl = signals.Dequeue();
+                ThrdSgnl.thread.HandleSignal(ThrdSgnl.signal);
+            }
+        }
+
+        public static void SendSignal(int tid, string signal)
+        {
+            for (int i = 0; i < threads.Count; i++)
+            {
+                if (threads[i].TID == tid)
+                {
+                    signals.Enqueue(new ThreadSignal(threads[i], signal));
+                    ProcessSignals();
+                    return;
+                }
+            }
+        }
+        public static void SendSignal(string threadName, string signal)
+        {
+            for (int i = 0; i < threads.Count; i++)
+            {
+                if (threads[i].name == threadName)
+                {
+                    SendSignal(threads[i].TID, signal);
+                    return;
+                }
+            }
+        }
+
+        public static void HandleRequest(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Alve Operating System/Alve OS/System/Threading/ThreadSignal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.System.Threading
+{
+    public class ThreadSignal
+    {
+        public Thread thread;
+        public string signal;
+        public ThreadSignal(Thread thrd, string sgnl)
+        {
+            this.thread = thrd;
+            this.signal = sgnl;
+        }
+    }
+}
