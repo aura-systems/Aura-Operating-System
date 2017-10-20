6d8ac5938ba4b50e43982f1878165807d79524e7
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-10-20 22:02:24 +0200

    Fixes.

--- Aura Operating System/Aura OS/Shell/cmdIntr/Settings/Settings.cs
+++ Aura Operating System/Aura OS/Shell/cmdIntr/Settings/Settings.cs
@@ -98,21 +98,13 @@ namespace Aura_OS.Shell.cmdIntr.Settings
                 }
             }
 
-            else if (cmdargs[1].StartsWith("textcolor "))
-            {
-                c_Console.TextColor.c_TextColor(cmdargs[2]);
-            }
             else if (cmdargs[1].Equals("textcolor"))
             {
-                c_Console.TextColor.c_TextColor();
-            }
-            else if (cmdargs[1].StartsWith("backgroundcolor "))
-            {
-                c_Console.BackGroundColor.c_BackGroundColor(cmdargs[2]);
+                  c_Console.TextColor.c_TextColor(cmdargs[2]);
             }
             else if (cmdargs[1].Equals("backgroundcolor"))
             {
-                c_Console.BackGroundColor.c_BackGroundColor();
+                c_Console.BackGroundColor.c_BackGroundColor(cmdargs[2]);
             }
 
             else
