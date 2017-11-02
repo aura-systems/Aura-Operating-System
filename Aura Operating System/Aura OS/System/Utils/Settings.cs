114425c901cdd679cd41f8c5386c22c9c55948ee
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-11-02 13:34:39 +0100

    Settings class.

---
+++ Aura Operating System/Aura OS/System/Utils/Settings.cs
@@ -0,0 +1,33 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Settings class
+* PROGRAMMERS:      Valentin Charbonnier <valentinbreiz@gmail.com>
+*/
+
+namespace Aura_OS.System.Utils
+{
+    public static class Settings
+    {
+
+        public static void PutValue()
+        {
+
+        }
+
+        public static void GetValue()
+        {
+
+        }
+
+        public static void EditValue()
+        {
+
+        }
+
+        public static void DeleteValue()
+        {
+
+        }
+
+    }
+}
