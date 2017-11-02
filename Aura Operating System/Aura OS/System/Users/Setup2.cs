1d36d137b975f7ad21ddb33754b0e0f7fc47d4f9
GeomTech <dacruzalexy@gmail.com>
2017-11-02 14:15:39 +0100

    Init

---
+++ Aura Operating System/Aura OS/System/Users/Setup2.cs
@@ -0,0 +1,46 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Setup
+* PROGRAMMERS:      Alexy DA CRUZ <dacruzalexy@gmail.com>
+*                   Valentin Charbonnier <valentinbreiz@gmail.com>
+*/
+
+using System;
+using System.IO;
+using Aura_OS.System.Security;
+using Aura_OS.System.Computer;
+using Aura_OS.System.Drawable;
+using Aura_OS.System.Translation;
+
+namespace Aura_OS.System
+{
+    class Setup2
+    {
+
+        public void isInstalled()
+        {
+            
+        }
+
+        #region Defaults
+
+        public void InitDefaults(string user)
+        {
+            string[] DefaultDirctories =
+            {
+                @"0:\Users\" + user +  @"\Desktop",
+                @"0:\Users\" + user +  @"\Documents",
+                @"0:\Users\" + user +  @"\Downloads",
+                @"0:\Users\" + user +  @"\Music"
+            };
+            foreach (string dirs in DefaultDirctories)
+            {
+                if (!Directory.Exists(dirs))
+                    Directory.CreateDirectory(dirs);
+            }
+        }
+
+        #endregion Defaults
+
+    }
+}
