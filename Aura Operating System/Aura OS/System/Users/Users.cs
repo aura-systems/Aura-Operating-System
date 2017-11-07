0ba5994f79df872719c45525f56a9c3139c477c3
GeomTech <dacruzalexy@gmail.com>
2017-11-07 23:09:34 +0100

    Code optimized.

--- Aura Operating System/Aura OS/System/Users/Users.cs
+++ Aura Operating System/Aura OS/System/Users/Users.cs
@@ -15,175 +15,6 @@ namespace Aura_OS.System.Users
 {
     class Users
     {
-
-        /// <summary>
-        /// Display login form
-        /// </summary>
-        public void Login()
-        {
-            switch (Kernel.langSelected)
-            {
-                case "fr_FR":
-                    string text = Menu.DispLoginForm("Connexion à votre compte Aura.");
-
-                    int middle = text.IndexOf("//////");
-                    string user = text.Remove(middle, text.Length - middle);
-                    string pass = text.Remove(0, middle + 6);
-
-                    string md5psw = MD5.hash(pass);
-
-                    string uservaluetosplit = Settings.GetUser("user:" + user);
-
-                    Char twodots = ':';
-
-                    string[] userarray = uservaluetosplit.Split(twodots);
-
-                    if (Settings.GetUser("user:" + user) != "null")
-                    {
-
-                        /* 1 = md5 password
-                         * 2 = level
-                         */
-
-                        if (md5psw == userarray[0])
-                        {
-                            //LOGGED
-                            Kernel.userLogged = user;
-                            UserLevel.LevelReader(userarray[1]);
-                            //InitUserDirs(user);
-                            Console.Clear();
-                            WelcomeMessage.Display();
-                            Text.Display("logged", user);
-                            Console.WriteLine("");
-                            Kernel.Logged = true;
-                        }
-                        else
-                        {
-                            Menu.DispErrorDialog("Mauvais mot de passe.");
-                            Login();
-                        }
-
-                    }
-                    else if (Settings.GetUser("user:root") != "null")
-                    {
-
-                        /* 1 = md5 password
-                         * 2 = level
-                         */
-
-                        if (md5psw == userarray[0])
-                        {
-                            //LOGGED
-                            Kernel.userLogged = user;
-                            UserLevel.LevelReader(userarray[1]);
-                            Kernel.userLevelLogged = UserLevel.Administrator();
-                            //InitUserDirs(user);
-                            Console.Clear();
-                            WelcomeMessage.Display();
-                            Text.Display("logged", user);
-                            Console.WriteLine("");
-                            Kernel.Logged = true;
-                        }
-                        else
-                        {
-                            Menu.DispErrorDialog("Mauvais mot de passe.");
-                            Login();
-                        }
-
-                    }
-                    else
-                    {
-                        Menu.DispErrorDialog("Utilisateur inconnu.");
-                        Login();
-                        break;
-                    }
-
-
-
-                    break;
-
-                case "en_US":
-
-                    string text1 = Menu.DispLoginForm("Login to your Aura account.");
-
-                    int middle1 = text1.IndexOf("//////");
-                    string user1 = text1.Remove(middle1, text1.Length - middle1);
-                    string pass1 = text1.Remove(0, middle1 + 6);
-
-                    string md5psw1 = MD5.hash(pass1);
-
-                    string uservaluetosplit1 = Settings.GetUser("user:" + user1);
-
-                    Char twodots1 = ':';
-
-                    string[] userarray1 = uservaluetosplit1.Split(twodots1);
-
-                    if (Settings.GetUser("user:" + user1) != "null")
-                    {
-
-                        /* 1 = md5 password
-                         * 2 = level
-                         */
-
-                        if (md5psw1 == userarray1[0])
-                        {
-                            //LOGGED
-                            Kernel.userLogged = user1;
-                            UserLevel.LevelReader(userarray1[1]);
-                            //InitUserDirs(user1);
-                            Console.Clear();
-                            WelcomeMessage.Display();
-                            Text.Display("logged", user1);
-                            Console.WriteLine("");
-                            Kernel.Logged = true;
-                        }
-                        else
-                        {
-                            Menu.DispErrorDialog("Wrong password.");
-                            Login();
-                        }
-
-                    }
-                    else if (Settings.GetUser("user:root") != "null")
-                    {
-
-                        /* 1 = md5 password
-                         * 2 = level
-                         */
-
-                        if (md5psw1 == userarray1[0])
-                        {
-                            //LOGGED
-                            Kernel.userLogged = user1;
-                            UserLevel.LevelReader(userarray1[1]);
-                            Kernel.userLevelLogged = UserLevel.Administrator();
-                            //InitUserDirs(user1);
-                            Console.Clear();
-                            WelcomeMessage.Display();
-                            Text.Display("logged", user1);
-                            Console.WriteLine("");
-                            Kernel.Logged = true;
-                        }
-                        else
-                        {
-                            Menu.DispErrorDialog("Wrong password.");
-                            Login();
-                        }
-
-                    }
-                    else
-                    {
-                        Menu.DispErrorDialog("Unknow user.");
-                        Login();
-                        break;
-                    }
-
-
-
-                    break;
-            }
-        }
-
         #region UserDirs
         public void InitUserDirs(string user)
         {
@@ -191,114 +22,47 @@ namespace Aura_OS.System.Users
             {
                 string[] RootDirectories =
                 {
-                @"0:\Users\" + user + @"\Root"
+                    @"0:\Users\" + user + @"\root"
                 };
                 foreach (string dirs in RootDirectories)
                     if (!Directory.Exists(dirs))
                         Directory.CreateDirectory(dirs);
                 return;
             }
-
-            string[] DefaultDirctories =
+            else
             {
-                @"0:\Users\" + user + @"\Desktop",
-                @"0:\Users\" + user + @"\Documents",
-                @"0:\Users\" + user + @"\Downloads",
-                @"0:\Users\" + user + @"\Music",
-            };
-            foreach (string dirs in DefaultDirctories)
-                if (!Directory.Exists(dirs))
-                    Directory.CreateDirectory(dirs);
+                string[] DefaultDirectories =
+                {
+                    @"0:\Users\" + user + @"\Desktop",
+                    @"0:\Users\" + user + @"\Documents",
+                    @"0:\Users\" + user + @"\Downloads",
+                    @"0:\Users\" + user + @"\Music",
+                };
+                foreach (string dirs in DefaultDirectories)
+                    if (!Directory.Exists(dirs))
+                        Directory.CreateDirectory(dirs);
+            }            
         }
         #endregion UserDirs
 
-        /// <summary>
-        /// Method called to create an user
-        /// </summary>
-        /// <param name="username">User name</param>
-        /// <param name="userlevel">User type</param>
-        public void Create(string username)
+
+        public void Create(string username, string password, string type = "standard")
         {
             try
             {
-                Console.WriteLine();
-                Text.Display("chooseyourusername");
-                Console.WriteLine();
-                Text.Display("user");
-                username = Console.ReadLine();
-
-                if (File.Exists(@"0:\System\Users\" + username + ".usr"))
+                password = MD5.hash(password);
+                Settings.LoadUsers();
+                if (Settings.GetUser("user").StartsWith(username))
                 {
-                    Text.Display("alreadyuser");
-                    Create(username);
+                    Console.WriteLine($"[NotTranslated] {username} exist already !");
+                    return;
                 }
-                else
-                {
-                    if ((username.Length >= 4) && (username.Length <= 20))
-                    {
-                        Console.WriteLine();
-                        Text.Display("passuser", username);
+                Settings.PutUser("user:" + username, password + "|" + type);
+                Settings.PushUsers();
+                Console.WriteLine($"[NotTranslated] {username} has been created !");
 
-                        psw:
-
-                        Console.WriteLine();
-                        Text.Display("passwd");
-                        Console.WriteLine();
-
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        string clearpassword = Console.ReadLine();
-                        Console.ForegroundColor = ConsoleColor.White;
-                        if ((clearpassword.Length >= 6) && (clearpassword.Length <= 40))
-                        {
-                            string password = MD5.hash(clearpassword);
-                            Console.ForegroundColor = ConsoleColor.White;
-
-                            Console.WriteLine();
-
-                            File.Create(@"0:\System\Users\" + username + ".usr");
-                            Directory.CreateDirectory(@"0:\Users\" + username);
-
-                            if (File.Exists(@"0:\System\Users\" + username + ".usr"))
-                            {
-
-                                Console.WriteLine();
-                                Text.Display("whattypeuser");
-                                groups:
-                                Text.Display("groupsavailable");
-                                Console.WriteLine();
-
-                                string userlevel = Console.ReadLine();
-
-                                if (userlevel == UserLevel.Administrator())
-                                {
-                                    File.WriteAllText(@"0:\System\Users\" + username + ".usr", password + "|admin");
-                                }
-                                else if (userlevel == UserLevel.StandardUser())
-                                {
-                                    File.WriteAllText(@"0:\System\Users\" + username + ".usr", password + "|standard");
-                                }
-                                else
-                                {
-                                    Console.WriteLine();
-                                    goto groups;
-                                }
-                            }
-                            else
-                            {
-                                Text.Display("passwd");
-                            }
-                        }
-                        else
-                        {
-                            Text.Display("pswcharmin");
-                            goto psw;
-                        }
-                    }
-                    else
-                    {
-                        Text.Display("charmin");
-                    }
-                }
+                InitUserDirs(username);
+                Console.WriteLine("[NotTranslated] Personal directories has been created !");
             }
             catch
             {
