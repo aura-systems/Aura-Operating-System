52819b4e86440ee35a033ef10b5ad96e73e01ac9
djlw78 <djlw78@gmail.com>
2017-11-20 18:41:42 -0600

    Extended the table a bit
    Your welcome!

--- Aura Operating System/Aura OS/System/Utils/Convert.cs
+++ Aura Operating System/Aura OS/System/Utils/Convert.cs
@@ -10,6 +10,8 @@ namespace Aura_OS.System.Utils
         {
             switch (text)
             {
+                case '@':
+                    return 0x40;
                 case 'A':
                     return 0x41;
                 case 'B':
@@ -62,6 +64,79 @@ namespace Aura_OS.System.Utils
                     return 0x59;
                 case 'Z':
                     return 0x5A;
+                case '[':
+                    return 0x5B;
+                case '\\':
+                    return 0x5C;
+                case ']':
+                    return 0x5D;
+                case '^':
+                    return 0x5E;
+                case '_':
+                    return 0x5F;
+
+                case '`':
+                    return 0x60;
+                case 'a':
+                    return 0x61;
+                case 'b':
+                    return 0x62;
+                case 'c':
+                    return 0x63;
+                case 'd':
+                    return 0x64;
+                case 'e':
+                    return 0x65;
+                case 'f':
+                    return 0x66;
+                case 'g':
+                    return 0x67;
+                case 'h':
+                    return 0x68;
+                case 'i':
+                    return 0x69;
+                case 'j':
+                    return 0x6A;
+                case 'k':
+                    return 0x6B;
+                case 'l':
+                    return 0x6C;
+                case 'm':
+                    return 0x6D;
+                case 'n':
+                    return 0x6E;
+                case 'o':
+                    return 0x6F;
+                case 'p':
+                    return 0x70;
+                case 'q':
+                    return 0x71;
+                case 'r':
+                    return 0x72;
+                case 's':
+                    return 0x73;
+                case 't':
+                    return 0x74;
+                case 'u':
+                    return 0x75;
+                case 'v':
+                    return 0x76;
+                case 'w':
+                    return 0x77;
+                case 'x':
+                    return 0x78;
+                case 'y':
+                    return 0x79;
+                case 'z':
+                    return 0x7A;
+                case '{':
+                    return 0x7B;
+                case '|':
+                    return 0x7C;
+                case '}':
+                    return 0x7D;
+                case '~':
+                    return 0x7E;
 
                 default:
                     return 0x00;
