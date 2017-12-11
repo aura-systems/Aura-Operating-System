36042638ae5deb39a306a56b556a918d460b720e
GeomTech <dacruzalexy@gmail.com>
2017-12-11 22:38:59 +0100

    Bug command

---
+++ Aura Operating System/Aura OS/Shell/cmdIntr/Tools/Bug.cs
@@ -0,0 +1,41 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Command Interpreter - Bug
+* PROGRAMMER(S):    DA CRUZ Alexy <dacruzalexy@gmail.com>
+*/
+
+using Aura_OS.Apps.User;
+using System;
+using System.IO;
+using L = Aura_OS.System.Translation;
+
+namespace Aura_OS.Shell.cmdIntr.Tools
+{
+    class Bug
+    {
+        private static string HelpInfo = "";
+
+        /// <summary>
+        /// Getter and Setters for Help Info.
+        /// </summary>
+        public static string HI
+        {
+            get { return HelpInfo; }
+            set { HelpInfo = value; /*PUSHED OUT VALUE (in)*/}
+        }
+
+        /// <summary>
+        /// Empty constructor. (Good for debug)
+        /// </summary>
+        public Bug() { }
+
+        /// <summary>
+        /// c = command, c_Bug
+        /// </summary>
+        public static void c_Bug()
+        {
+            System.Utils.Bug bug = new System.Utils.Bug();
+            bug.ScanIssues();
+        }
+    }
+}
