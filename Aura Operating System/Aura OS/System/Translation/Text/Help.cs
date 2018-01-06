0fa1e0d1362b404c176ae30f384ab8135cd5832e
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-01-06 21:15:20 +0100

    Crashs.

--- Aura Operating System/Aura OS/System/Translation/Text/Help.cs
+++ Aura Operating System/Aura OS/System/Translation/Text/Help.cs
@@ -48,6 +48,8 @@ namespace Aura_OS.System.Translation
                     Console.WriteLine("- ipconfig (affiche les informations réseau)");
                     Console.WriteLine("- snake (lance le jeu Snake)");
                     Console.WriteLine("- md5 (affiche le hash des arguments)");
+                    Console.WriteLine("- crash (crash Aura)");
+                    Console.WriteLine("- crashcpu (crash CPU)");
 
                     Console.ReadKey();
 
@@ -83,6 +85,8 @@ namespace Aura_OS.System.Translation
                     Console.WriteLine("- ipconfig (to get network information)");
                     Console.WriteLine("- snake (launch the game Snake)");
                     Console.WriteLine("- md5 (to display hash of arguments)");
+                    Console.WriteLine("- crash (crash Aura)");
+                    Console.WriteLine("- crashcpu (crash CPU)");
 
                     Console.ReadKey();
 
