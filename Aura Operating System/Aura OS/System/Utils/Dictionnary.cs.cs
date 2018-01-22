c708b559744de81e9b5f9f89c8e8d4932e8ee25f
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-01-22 12:03:05 +0100

    Forgot that.

---
+++ Aura Operating System/Aura OS/System/Utils/Dictionnary.cs.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.System.Utils
+{
+    public class Dictionary<TKey, TValue> : IEnumerable
+    {
+        public List<TKey> Keys = new List<TKey>();
+        public List<TValue> Values = new List<TValue>();
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return Get(key);
+            }
+            set
+            {
+                Values[Keys.IndexOf(key)] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return Keys.Count;
+            }
+        }
+
+        public bool ContainsKey(TKey k)
+        {
+            return Keys.Contains(k);
+        }
+
+        public TValue Get(TKey key)
+        {
+            int index = Keys.IndexOf(key);
+            return Values[index];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Keys.Add(key);
+            Values.Add(value);
+        }
+
+        public TKey[] GetKeys()
+        {
+            return Keys.ToArray();
+        }
+
+        public TValue[] GetValues()
+        {
+            return Values.ToArray();
+        }
+
+        public TKey GetKeyByValue(TValue value)
+        {
+            var x = Values.IndexOf(value);
+            var x_ = Keys[x];
+            return x_;
+        }
+
+        public void Remove(TKey key)
+        {
+            int index = Keys.IndexOf(key);
+            Keys.RemoveAt(index);
+            Values.RemoveAt(index);
+        }
+
+        public void Clear()
+        {
+            Keys = new List<TKey>();
+            Values = new List<TValue>();
+        }
+
+        /// <summary>
+        /// Get Values
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator_V()
+        {
+            return ((IEnumerable)Values).GetEnumerator();
+        }
+
+        /// <summary>
+        /// Default GetEnumerator (Keys)
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Keys).GetEnumerator();
+        }
+
+    }
+}
