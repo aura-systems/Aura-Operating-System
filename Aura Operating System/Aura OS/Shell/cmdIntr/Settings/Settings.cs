bfec049fdbac2e3ba1f0358fa6c21668de898028
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-01-22 11:42:49 +0000

    Update Settings.cs

--- Aura Operating System/Aura OS/Shell/cmdIntr/Settings/Settings.cs
+++ Aura Operating System/Aura OS/Shell/cmdIntr/Settings/Settings.cs
@@ -70,7 +70,7 @@ namespace Aura_OS.Shell.cmdIntr.Settings
                     L.Text.Display("_passuser");
                 }
 
-    				    else if (cmdargs[1].Equals("safemode"))
+    		else if (cmdargs[1].Equals("safemode"))
                 {
                     L.Text.Display("safemode");
                 }
@@ -85,22 +85,22 @@ namespace Aura_OS.Shell.cmdIntr.Settings
                     users.Remove(cmdargs[2]);
                 }
 
-				        else if (cmdargs[1].Equals("safemode"))
+		else if (cmdargs[1].Equals("safemode"))
                 {
                     if (cmdargs[2].Equals("enable"))
-					          {
-						            Aura_OS.Kernel.Safemode = true;
-						            L.Text.Display("safemodeenabled");
-					          }
-          					else if (cmdargs[2].Equals("disable")
-          					{
-          					    Aura_OS.Kernel.Safemode = false;
-          					    L.Text.Display("safemodedisabled");
-          					}
-          					else
-          					{
-          					    L.Text.Display("UnknownCommand");
-          					}
+		    {
+		        Aura_OS.Kernel.Safemode = true;
+			L.Text.Display("safemodeenabled");
+		    }
+          	    else if (cmdargs[2].Equals("disable")
+          	    {
+          	        Aura_OS.Kernel.Safemode = false;
+          		L.Text.Display("safemodedisabled");
+          	    }
+          	    else
+          	    {
+          		L.Text.Display("UnknownCommand");
+          	    }
                 }
 
                 else if (cmdargs[1].Equals("setlang"))
@@ -165,9 +165,9 @@ namespace Aura_OS.Shell.cmdIntr.Settings
                     L.Text.Display("remuser");
                 }
 
-				        else if (cmdargs[1].Equals("safemode"))
+		else if (cmdargs[1].Equals("safemode"))
                 {
-					          L.Text.Display("safemode");
+		    L.Text.Display("safemode");
                 }
             }
 
