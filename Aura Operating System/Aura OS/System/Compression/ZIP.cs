608993b38b0fb04ad6d2badaae41efeab46a51be
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-04-14 19:43:18 +0200

    Adding filename from fileheader & tests

--- Aura Operating System/Aura OS/System/Compression/ZIP.cs
+++ Aura Operating System/Aura OS/System/Compression/ZIP.cs
@@ -14,7 +14,6 @@ namespace Aura_OS.System.Compression
             ZIPFile = filename;
         }
 
-
         private Byte[] FileHeader()
         {
             Byte[] zip = File.ReadAllBytes(ZIPFile);
@@ -41,15 +40,30 @@ namespace Aura_OS.System.Compression
             return false;
         }
 
+        private string FileName()
+        {
+            Byte[] zip = FileHeader();
+            List<Byte> bname = new List<Byte>();            
+            int lenght = zip[26];
+            Console.WriteLine(lenght);
+            Console.WriteLine(zip[30]);
+            for (int i = 30; i < lenght; i++){
+                bname.Add(zip[i]);
+                Console.WriteLine(zip[i]);
+            }
+            return Encoding.ASCII.GetString(bname.ToArray());
+        }
+
         public void Open()
         {
             Byte[] zip = FileHeader();
             if (IsZIPFile()) //if it's a zip file
             {
-                if (IsVersionNeeded()) //if the zip file is supported
-                {
-
-                }
+                //if (IsVersionNeeded()) //if the zip file is supported
+                //{
+                //    Console.WriteLine(FileName());
+                //}
+                Console.WriteLine(FileName());
             }
         }
     }
