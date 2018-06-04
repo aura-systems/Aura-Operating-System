25047732c1be106c51a1b368f674ddd020443a23
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-06-04 16:27:04 +0200

    Bug fix.

--- Aura Operating System/Aura_Plugs/HAL/Global.cs
+++ Aura Operating System/Aura_Plugs/HAL/Global.cs
@@ -33,12 +33,6 @@ namespace Aura_Plugs.HAL
             ACPI.Start();
             CustomConsole.WriteLineOK("ACPI Initialization");
 
-            Cosmos.HAL.BlockDevice.IDE.InitDriver();
-            CustomConsole.WriteLineOK("IDE Driver Initialization");
-
-            Cosmos.HAL.BlockDevice.AHCI.InitDriver();
-            CustomConsole.WriteLineOK("AHCI Driver Initialization");
-
             CustomConsole.WriteLineOK("Kernel successfully initialized!");
 
         }
