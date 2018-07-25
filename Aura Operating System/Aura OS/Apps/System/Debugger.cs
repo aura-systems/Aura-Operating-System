f233f32d5f0ac01b4d64422f30dad8ce751f253f
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-07-25 20:38:46 +0200

    Error dialog.

--- Aura Operating System/Aura OS/Apps/System/Debugger.cs
+++ Aura Operating System/Aura OS/Apps/System/Debugger.cs
@@ -96,6 +96,7 @@ namespace Aura_OS.Apps.System
             }
             else if (result.Equals("changeip"))
             {
+
                 string ip = Aura_OS.System.Drawable.Menu.DispDialogOneArg("Change IP address (currently " + Kernel.debugger.ip.ToString() +")", "IP Address: ");
 
                 if (Aura_OS.System.Utils.Misc.IsIpv4Address(ip))
@@ -104,6 +105,7 @@ namespace Aura_OS.Apps.System
                 }
                 else
                 {
+                    Aura_OS.System.Drawable.Menu.DispErrorDialog("It is not an IP address!");
                     RegisterSetting();
                 }
             }
