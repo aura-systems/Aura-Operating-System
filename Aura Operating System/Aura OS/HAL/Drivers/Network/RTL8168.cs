09e97a2ba61f151aa60ae51b077da9acb5ed03a4
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-08-29 23:37:04 +0200

    Initialization.

--- Aura Operating System/Aura OS/HAL/Drivers/Network/RTL8168.cs
+++ Aura Operating System/Aura OS/HAL/Drivers/Network/RTL8168.cs
@@ -14,7 +14,7 @@ using static Cosmos.Core.INTs;
 
 namespace Aura_OS.HAL.Drivers.Network
 {
-    public class RTL8168 : NetworkDevice
+    public unsafe class RTL8168 : NetworkDevice
     {
         protected PCIDevice pciCard;
         protected AMDPCNetIIIOGroup io;
@@ -34,7 +34,113 @@ namespace Aura_OS.HAL.Drivers.Network
 
         public override CardType CardType => CardType.Ethernet;
 
+        public struct rx_desc
+        {
+            public ushort buffer_size;
+            public ushort reserved;
+            public int eor; // if set end of ring
+            public int own; // if 1 owned by nic / else owned by host
+            public uint vlan;
+            public uint addr_low;
+            public uint addr_high;
+        }
+
+        public struct rx_desc_status
+        {
+            public ushort frame_length; // if own = 0 and ls = 1 -> packet length incl. crc in bytes
+            public int tcpf; // if set -> tcp checksum failure
+            public int udpf; // if set -> udp checksum failure
+            public int ipf; // if set -> ip checksum failure
+            public int pid; // protocol-ID:
+                            /*
+                            00 = IP
+                            01 = TCP/IP
+                            10 = UDP/IP
+                            11 = IP
+                            */
+            public int crc; // if set -> crc-error
+            public int runt; // packet smaller than 64 bytes
+            public int res; // if set and ls=1 -> error (crc,runt,rwt,fae)
+            public int rwt; // packet bigger than 8192 bytes
+            public int reserved; // always 0x01
+            public int bar; // broadcast packet received
+            public int pam; // physical packet received
+            public int mar; // multicast packet received
+            public int ls; // if set this is the last segment of packet
+            public int fs; // if set this is the first segment of packet
+            public int eor; // if set end of ring
+            public int own; // if 1 owned by nic / else owned by host
+            public uint vlan;
+            public uint addr_low;
+            public uint addr_high;
+        }
+
+        struct tx_desc
+        {
+            public ushort frame_length; // if own = 0 and ls = 1 -> packet length incl. crc in bytes
+            public int tcpcs; // if set -> auto checksum
+            public int udpcs; // if set -> auto checksum
+            public int ipcs; // if set -> auto checksum
+            public int reserved; // Reserved
+            public int lgsen; // Large-send
+            public int ls; // if set this is the last segment of packet
+            public int fs; // if set this is the first segment of packet
+            public int eor; // if set end of ring
+            public int own; // if 1 owned by nic / else owned by host
+            public uint vlan;
+            public uint addr_low;
+            public uint addr_high;
+        }
+
+        rx_desc* rx_descs;
+        tx_desc* tx_descs;
+
+        byte*[] rx_buf = new byte*[10];
+        byte*[] tx_buf = new byte*[10];
+
+        int realtek_next_tx = 0;
+
         public override string Name => "RTL8168";
+
+        public static class Size
+        {
+            public static int Of(uint x)
+            {
+                return sizeof(uint);
+            }
+
+        }
+
+        void init_buffers()
+        {
+            rx_descs = (rx_desc*)Cosmos.Core.Memory.Old.Heap.MemAlloc((uint)rx_descs);
+            tx_descs = (tx_desc*)Cosmos.Core.Memory.Old.Heap.MemAlloc((uint)tx_descs);
+            for (int i = 0; i < 10; i++)
+            {
+                rx_buf[i] = (byte*)0;
+                tx_buf[i] = (byte*)0;
+
+                rx_descs[i].own = 1;
+                rx_descs[i].eor = 0;
+                rx_descs[i].buffer_size = 0x0FFF;
+                rx_descs[i].addr_low = (uint)rx_buf[i];
+                rx_descs[i].addr_high = 0;
+
+                tx_descs[i].own = 0;
+                tx_descs[i].eor = 0;
+                tx_descs[i].fs = 0;
+                tx_descs[i].ls = 0;
+                tx_descs[i].lgsen = 0;
+                tx_descs[i].ipcs = 0;
+                tx_descs[i].udpcs = 0;
+                tx_descs[i].tcpcs = 0;
+                tx_descs[i].frame_length = 0x0FFF;
+                tx_descs[i].addr_low = (uint)tx_buf[i];
+                tx_descs[i].addr_high = 0;
+            }
+            rx_descs[9].eor = 1;
+            tx_descs[9].eor = 1;
+        }
 
         public RTL8168(PCIDevice device)
         {
@@ -62,6 +168,20 @@ namespace Aura_OS.HAL.Drivers.Network
             }
 
             this.mac = new MACAddress(eeprom_mac);
+
+            init_buffers();
+
+            Ports.outd((ushort)(BaseAddress + 0x44), 0x0000E70F);
+            Ports.outb((ushort)(BaseAddress + 0x37), 0x04); // Enable TX
+            Ports.outd((ushort)(BaseAddress + 0x40), 0x03000700);
+            Ports.outw((ushort)(BaseAddress + 0xDA), 0x0FFF); // Maximal 8kb-Pakete
+            Ports.outb((ushort)(BaseAddress + 0xEC), 0x3F); // No early transmit
+
+            Ports.outd((ushort)(BaseAddress + 0x20), (uint)tx_descs);
+            Ports.outd((ushort)(BaseAddress + 0xE4), (uint)rx_descs);
+
+            Ports.outw((ushort)(BaseAddress + 0x3C), 0x03FF); //Activating all Interrupts
+            Ports.outb((ushort)(BaseAddress + 0x37), 0x0C); // Enabling receive and transmit
 
         }
 
