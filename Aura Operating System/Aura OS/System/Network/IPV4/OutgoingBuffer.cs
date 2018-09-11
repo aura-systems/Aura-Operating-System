4089316fae3a4411d77dc012ed1eb0410cdf2b70
Alexy DA CRUZ (GeomTech) <geomtech@users.noreply.github.com>
2018-09-11 20:06:02 +0200

    Remove the last debug line

--- Aura Operating System/Aura OS/System/Network/IPV4/OutgoingBuffer.cs
+++ Aura Operating System/Aura OS/System/Network/IPV4/OutgoingBuffer.cs
@@ -105,8 +105,6 @@ namespace Aura_OS.System.Network.IPV4
                         //Need to figure how this is working
                         if (Config.IsLocalAddress(entry.Packet.DestinationIP) == false)
                         {
-                            CustomConsole.WriteLineInfo("Destination IP is out of the network!");
-
                             entry.nextHop = Config.FindRoute(entry.Packet.DestinationIP);
                             if (entry.nextHop == null)
                             {
