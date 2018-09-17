bf530ee2ea83409862127b6a68538dd3463f79a2
Alexy DA CRUZ (GeomTech) <geomtech@users.noreply.github.com>
2018-09-17 22:45:32 +0200

    TCP Rules file manager

---
+++ Aura Operating System/Aura OS/System/Network/Firewall/TCPRules.cs
@@ -0,0 +1,142 @@
+﻿using Aura_OS.System.Translation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Aura_OS.System.Network.Firewall
+{
+    class TCPRules
+    {
+        public static string[] rules;
+        static string[] reset;
+        static List<string> rulesfile = new List<string>();
+
+        /// <summary>
+        /// Method to create an user.
+        /// </summary>
+        public static void Create(string ip, ushort port, bool incoming, bool outgoing)
+        {
+            try
+            {
+                LoadRules();
+                AddRule(ip, port, incoming, outgoing);
+                PushRules();
+            }
+            catch
+            {
+                //Text.Display("errorwhileusercreating");
+            }
+        }
+
+
+        public static void DeleteRule(string ip, ushort port, bool incoming, bool outgoing)
+        {
+
+            foreach (string line in rules)
+            {
+                rulesfile.Add(line);
+            }
+
+            int counter = -1;
+            int index = 0;
+
+            bool exists = false;
+
+            foreach (string element in rulesfile)
+            {
+                counter = counter + 1;
+                if (element.Contains(ip + ":" + port.ToString()))
+                {
+                    index = counter;
+                    exists = true;
+                }
+            }
+            if (exists)
+            {
+                rulesfile.RemoveAt(index);
+
+                rules = rulesfile.ToArray();
+
+                rulesfile.Clear();
+
+                File.Delete(@"0:\System\firewall_tcp.conf");
+
+                PushRules();
+            }
+        }
+
+
+        public static void DeleteRule(string rule)
+        {
+
+            foreach (string line in rules)
+            {
+                rulesfile.Add(line);
+            }
+
+            int counter = -1;
+            int index = 0;
+
+            bool exists = false;
+
+            foreach (string element in rulesfile)
+            {
+                counter = counter + 1;
+                if (element.Contains(rule))
+                {
+                    index = counter;
+                    exists = true;
+                }
+            }
+            if (exists)
+            {
+                rulesfile.RemoveAt(index);
+
+                rules = rulesfile.ToArray();
+
+                rulesfile.Clear();
+
+                File.Delete(@"0:\System\firewall_tcp.conf");
+
+                PushRules();
+            }
+        }
+
+        public static void AddRule(string ip, ushort port, bool incoming, bool outgoing)
+        {
+            bool contains = false;
+
+            foreach (string line in rules)
+            {
+                rulesfile.Add(line);
+                if (line.StartsWith(ip + ":" + port.ToString() + "," + incoming.ToString() + "," + outgoing.ToString()))
+                {
+                    contains = true;
+                }
+            }
+
+            if (!contains)
+            {
+                rulesfile.Add(ip + ":" + port.ToString() + "," + incoming.ToString() + "," + outgoing.ToString());
+            }
+
+            rules = rulesfile.ToArray();
+
+            rulesfile.Clear();
+        }
+
+        public static void PushRules()
+        {
+            File.WriteAllLines(@"0:\System\firewall_tcp.conf", rules);
+        }
+
+        public static void LoadRules()
+        {
+            //reset of users string array in memory if there is "something"
+            rules = reset;
+            //load
+            rules = File.ReadAllLines(@"0:\System\firewall_tcp.conf");
+        }
+    }
+}
