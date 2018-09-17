e54682aac83b693bfda2efb82777046b17b24d30
Alexy DA CRUZ (GeomTech) <geomtech@users.noreply.github.com>
2018-09-17 22:45:54 +0200

    Firewall Create Rule Test

--- Aura Operating System/Aura OS/Shell/cmdIntr/CommandManager.cs
+++ Aura Operating System/Aura OS/Shell/cmdIntr/CommandManager.cs
@@ -312,6 +312,11 @@ namespace Aura_OS.Shell.cmdIntr
                 Console.WriteLine(new System.Network.IPV4.Address(192, 168, 1, 12).Hash);
             }
 
+            else if (cmd.Equals("firewall-create"))
+            {
+                System.Network.Firewall.TCPRules.Create("192.168.1.59", 4224, true, true);
+            }
+
             //else if (cmd.Equals("discover"))
             //{
             //    //byte[] mac = { 0x00,0x0C, 0x29,0x7C, 0x85,0x28};                
