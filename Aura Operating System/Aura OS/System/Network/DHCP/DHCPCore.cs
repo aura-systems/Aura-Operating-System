4f3b3097da536218a4ef0c0f4a0e86bd61149b5e
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-09-26 22:49:10 +0200

    Options in DHCP Core class

---
+++ Aura Operating System/Aura OS/System/Network/DHCP/DHCPCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.System.Network.DHCP
+{
+    class DHCPCore
+    {
+        public static byte DHCPTypeLength(byte[] dhcpPacket)
+        {
+            return 0x0;
+        }
+    }
+}
