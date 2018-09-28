26256e61723b483979206ffe2c1a3ad7b808465f
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-09-28 23:11:48 +0200

    DHCPRelease packet

--- Aura Operating System/Aura OS/System/Network/DHCP/DHCPRelease.cs
+++ Aura Operating System/Aura OS/System/Network/DHCP/DHCPRelease.cs
@@ -14,18 +14,10 @@ namespace Aura_OS.System.Network.DHCP
     {
         protected int xID;
 
-        public DHCPRelease()
-            : base()
-        { }
-
-        public DHCPRelease(byte[] rawData)
-            : base(rawData)
-        { }
-
         public static int PacketSize { get; set; }
 
-        public DHCPRelease(MACAddress src, Address source, Address requested_ip)
-            : base(src, source, requested_ip)
+        public DHCPRelease(Address source, Address dhcp_server)
+            : base(source)
         {
             //Request
             mRawData[dataOffset + 8] = 0x01;
@@ -47,20 +39,31 @@ namespace Aura_OS.System.Network.DHCP
             mRawData[dataOffset + 14] = (byte)((xID >> 8) & 0xFF);
             mRawData[dataOffset + 15] = (byte)((xID >> 0) & 0xFF);
 
+            //sec elapsed & bootp
+            mRawData[dataOffset + 16] = 0x00;
+            mRawData[dataOffset + 17] = 0x00;
+            mRawData[dataOffset + 18] = 0x00;
+            mRawData[dataOffset + 19] = 0x00;
+
+            //client IP addr
+            mRawData[dataOffset + 20] = source.address[0];
+            mRawData[dataOffset + 21] = source.address[1];
+            mRawData[dataOffset + 22] = source.address[2];
+            mRawData[dataOffset + 23] = source.address[3];
 
             //option bootp
-            for (int i = 16; i < 35; i++)
+            for (int i = 24; i < 35; i++)
             {
                 mRawData[dataOffset + i] = 0x00;
             }
 
-            //Src mac
-            mRawData[dataOffset + 36] = src.bytes[0];
-            mRawData[dataOffset + 37] = src.bytes[1];
-            mRawData[dataOffset + 38] = src.bytes[2];
-            mRawData[dataOffset + 39] = src.bytes[3];
-            mRawData[dataOffset + 40] = src.bytes[4];
-            mRawData[dataOffset + 41] = src.bytes[5];
+            //SourceMAC mac
+            mRawData[dataOffset + 36] = SourceMAC.bytes[0];
+            mRawData[dataOffset + 37] = SourceMAC.bytes[1];
+            mRawData[dataOffset + 38] = SourceMAC.bytes[2];
+            mRawData[dataOffset + 39] = SourceMAC.bytes[3];
+            mRawData[dataOffset + 40] = SourceMAC.bytes[4];
+            mRawData[dataOffset + 41] = SourceMAC.bytes[5];
 
             //Fill 0
             for (int i = 42; i < 243; i++)
@@ -76,37 +79,39 @@ namespace Aura_OS.System.Network.DHCP
 
             //options
 
-            //Discover
+            //Release
             mRawData[dataOffset + 248] = 0x35;
             mRawData[dataOffset + 249] = 0x01;
-            mRawData[dataOffset + 250] = 0x01;
+            mRawData[dataOffset + 250] = 0x07;
 
-            //Requested IP Address
-            mRawData[dataOffset + 251] = 0x32;
-            mRawData[dataOffset + 252] = 4;
+            //DHCP Server ID
+            mRawData[dataOffset + 251] = 0x36;
+            mRawData[dataOffset + 252] = 0x04;
 
-            mRawData[dataOffset + 253] = requested_ip.address[0];
-            mRawData[dataOffset + 254] = requested_ip.address[1];
-            mRawData[dataOffset + 255] = requested_ip.address[2];
-            mRawData[dataOffset + 256] = requested_ip.address[3];
+            mRawData[dataOffset + 253] = dhcp_server.address[0];
+            mRawData[dataOffset + 254] = dhcp_server.address[1];
+            mRawData[dataOffset + 255] = dhcp_server.address[2];
+            mRawData[dataOffset + 256] = dhcp_server.address[3];
 
-            //Parameters start here
-            mRawData[dataOffset + 257] = 0x37;
-            mRawData[dataOffset + 258] = 4;
+            //Client ID
+            mRawData[dataOffset + 257] = 0x3d;
+            mRawData[dataOffset + 258] = 7;
+            mRawData[dataOffset + 259] = 1;
 
-            //Parameters
-            mRawData[dataOffset + 259] = 0x01;
-            mRawData[dataOffset + 260] = 0x03;
-            mRawData[dataOffset + 261] = 0x0f;
-            mRawData[dataOffset + 262] = 0x06;
+            mRawData[dataOffset + 260] = SourceMAC.bytes[0];
+            mRawData[dataOffset + 261] = SourceMAC.bytes[1];
+            mRawData[dataOffset + 262] = SourceMAC.bytes[2];
+            mRawData[dataOffset + 263] = SourceMAC.bytes[3];
+            mRawData[dataOffset + 264] = SourceMAC.bytes[4];
+            mRawData[dataOffset + 265] = SourceMAC.bytes[5];
 
-            mRawData[dataOffset + 263] = 0xff; //ENDMARK
+            mRawData[dataOffset + 266] = 0xff; //ENDMARK
 
             //Fill 0
-            for (int i = 264; i < 272; i++)
-            {
-                mRawData[dataOffset + i] = 0x00;
-            }
+            //for (int i = 264; i < 272; i++)
+            //{
+            //    mRawData[dataOffset + i] = 0x00;
+            //}
 
             initFields();
         }
