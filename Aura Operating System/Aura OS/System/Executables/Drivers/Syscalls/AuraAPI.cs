1e6fd90e2e5a61cdbc91ebaba47f92940adc9666
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-10-02 16:15:43 +0200

    Tests

--- Aura Operating System/Aura OS/System/Executables/Drivers/Syscalls/AuraAPI.cs
+++ Aura Operating System/Aura OS/System/Executables/Drivers/Syscalls/AuraAPI.cs
@@ -2,48 +2,104 @@
 using static Cosmos.Core.INTs;
 using System.Collections.Generic;
 using System.Text;
+using Cosmos.Core;
 
 namespace Aura_OS.System.Executables.Drivers.Syscalls
 {
 	class AuraAPI
 	{
+	    public static ManagedMemoryBlock sharedmemory;
+
 		public static bool Init()
 		{
 			SetIntHandler(0x48, SWI); //ints.setinthandler
-			return true;
+		    sharedmemory = new ManagedMemoryBlock(2048);
+
+            return true;
 		}
 
 		public static int x = Console.CursorLeft;
 		public static int y = Console.CursorTop;
 
+	    enum SystemCalls
+	    {
+            SYS_OPEN = 0,
+            SYS_EXIT = 1,
+	        SYS_READ = 3,
+            SYS_WRITE = 4
+	    }
+
 		public unsafe static void SWI(ref IRQContext aContext)
 		{
 			if (aContext.Interrupt == 0x48) //Interrupt.
 			{
-			    Console.WriteLine("EAX=" + CosmosELFCore.Invoker.eax);
-                if (aContext.EAX == 1) //sys_exit
-                {
-                    Console.WriteLine("sys_exit");
-                }
-                else if (aContext.EAX == 4) //sys_write
-                {
-                    Console.WriteLine("sys_write");
-                    if (aContext.EBX == 1) //stdout
-                    {
-                        Console.WriteLine("sys_exit");
-                        uint maxBytes = aContext.EDX;
-                        uint ptr = aContext.ECX;
-                        byte* dat = (byte*)(ptr + Executables.PlainBinaryProgram.ProgramAddress); //
 
-                        for (int i = 0; dat[i] != 0 && i < maxBytes; i++)
+			    switch (aContext.EAX)
+			    {
+
+			        case (uint)SystemCalls.SYS_OPEN:
+			            Console.WriteLine("sys_open");
+			            Console.WriteLine("Offset= " + sharedmemory.Offset);
+                        aContext.ECX = sharedmemory.Offset;
+                        break;
+
+                    case (uint)SystemCalls.SYS_EXIT:
+                        Console.WriteLine("sys_exist");
+                        for (uint i = 0; i < sharedmemory.Size; i++)
                         {
-                            if ((char)dat[i] == 0x0A)
-                                Console.WriteLine("\n");
-                            else
-                                Console.Write((char)dat[i]);
+                            sharedmemory[i] = 0;
                         }
-                    }
-                }
+                        break;
+
+			        case (uint)SystemCalls.SYS_READ:
+			            Console.WriteLine("sys_read");
+			            if (aContext.EBX == 0) //stdin
+			            {
+			                //Console.WriteLine("stdin");
+			                uint Lenght = aContext.EDX;
+			                uint ptr = aContext.ECX;
+                            byte* data = (byte*)(ptr);
+
+                            for (int i = 0; i < Lenght; i++)
+			                {
+			                    ConsoleKeyInfo info = Console.ReadKey();
+			                    data[i] = (byte)info.KeyChar;
+                                Console.WriteLine();
+			                }
+
+			                aContext.ECX = (uint)data;
+
+                        }
+			            break;
+
+                    case (uint)SystemCalls.SYS_WRITE:
+                        Console.WriteLine("sys_write");
+                        if (aContext.EBX == 1) //stdout
+                        {
+                            Console.WriteLine("stdout");
+                            uint Lenght = aContext.EDX;
+                            Console.WriteLine("Lenght= " + Lenght);
+
+                            uint ptr = aContext.ECX;
+                            byte* dat = (byte*)(ptr + PlainBinaryProgram.ProgramAddress);
+
+                            for (uint i = 0; i < Lenght && dat[i] != 0; i++)
+                            {
+                                if ((char)dat[i] == 0x0A)
+                                    Console.Write('\n');
+                                else
+                                    Console.Write((char)dat[i]);
+                            }
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown SystemCall!");
+                        Console.WriteLine("EAX= " + aContext.EAX);
+                        break;
+
+			    }
+
 			}
 		}
 	}
