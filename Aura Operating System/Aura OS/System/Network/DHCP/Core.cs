70758b051f4cfb6f5b583db316332b9510bb73ce
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-10-29 21:35:52 +0100

    Adding comments & displaying or not messages for DHCP

--- Aura Operating System/Aura OS/System/Network/DHCP/Core.cs
+++ Aura Operating System/Aura OS/System/Network/DHCP/Core.cs
@@ -73,19 +73,31 @@ namespace Aura_OS.System.Network.DHCP
             }
         }
 
-        public static void Apply(DHCPOption Options)
+        /*
+         * Method called to applied the differents options received in the DHCP packet ACK
+         **/
+        /// <summary>
+        /// Apply the new IP configuration received.
+        /// </summary>
+        /// <param name="Options">DHCPOption class using the packetData from the received dhcp packet.</param>
+        /// <param name="message">Enable/Disable the displaying of messages about DHCP applying and conf. Disabled by default.
+        /// </param>
+        public static void Apply(DHCPOption Options, bool message = false)
         {
             NetworkStack.RemoveAllConfigIP();
 
-            //cf. Roadmap (have to change this, because some network interfaces are not configured in dhcp mode) [have to be done in 0.5.x]
+            //cf. Roadmap. (have to change this, because some network interfaces are not configured in dhcp mode) [have to be done in 0.5.x]
             foreach (NetworkDevice networkDevice in NetworkDevice.Devices)
             {
-                Console.WriteLine();
-                CustomConsole.WriteLineInfo("[DHCP ACK][" + networkDevice.Name + "] Packet received, applying IP configuration...");
-                CustomConsole.WriteLineInfo("   IP Address  : " + Options.Address().ToString());
-                CustomConsole.WriteLineInfo("   Subnet mask : " + Options.Subnet().ToString());
-                CustomConsole.WriteLineInfo("   Gateway     : " + Options.Gateway().ToString());
-                CustomConsole.WriteLineInfo("   DNS server  : " + Options.DNS01().ToString());
+                if (message)
+                {
+                    Console.WriteLine();
+                    CustomConsole.WriteLineInfo("[DHCP ACK][" + networkDevice.Name + "] Packet received, applying IP configuration...");
+                    CustomConsole.WriteLineInfo("   IP Address  : " + Options.Address().ToString());
+                    CustomConsole.WriteLineInfo("   Subnet mask : " + Options.Subnet().ToString());
+                    CustomConsole.WriteLineInfo("   Gateway     : " + Options.Gateway().ToString());
+                    CustomConsole.WriteLineInfo("   DNS server  : " + Options.DNS01().ToString());
+                }
 
                 Utils.Settings settings = new Utils.Settings(@"0:\System\" + networkDevice.Name + ".conf");
                 settings.EditValue("ipaddress", Options.Address().ToString());
@@ -97,9 +109,11 @@ namespace Aura_OS.System.Network.DHCP
 
                 NetworkInit.Enable();
 
-                CustomConsole.WriteLineOK("[DHCP CONFIG][" + networkDevice.Name + "] IP configuration applied.");
-                Console.WriteLine();
-
+                if (message)
+                {
+                    CustomConsole.WriteLineOK("[DHCP CONFIG][" + networkDevice.Name + "] IP configuration applied.");
+                    Console.WriteLine();
+                }
             }
 
             Kernel.BeforeCommand();
