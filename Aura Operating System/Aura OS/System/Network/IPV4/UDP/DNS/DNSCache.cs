cb40efa51fbcb5542706ec8f6cfa9c55a6a6d855
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-10-31 22:24:15 +0100

    DNS Cache implemented

---
+++ Aura Operating System/Aura OS/System/Network/IPV4/UDP/DNS/DNSCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.System.Network.IPV4.UDP.DNS
+{
+    class DNSCache
+    {
+        public static List<string> Name = new List<string>();
+        public static List<Address> Address = new List<Address>();
+
+        public static void AddEntry(string name, Address address)
+        {
+            Name.Add(name);
+            Address.Add(address);
+        }
+
+        public static Address Lookup(string name)
+        {
+            if (Name.Contains(name))
+            {
+                return Address[Name.IndexOf(name)];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string ReverseLookup(Address address)
+        {
+            if (Address.Contains(address))
+            {
+                return Name[Address.IndexOf(address)];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
