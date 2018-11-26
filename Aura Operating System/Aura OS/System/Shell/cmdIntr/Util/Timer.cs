272f145a6526d9c71f04a031457bf5ba63af1f65
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-11-26 23:48:51 +0100

    Create Timer.cs

---
+++ Aura Operating System/Aura OS/System/Shell/cmdIntr/Util/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.System.Shell.cmdIntr.Util
+{
+    class Timer
+    {
+        /// <summary>
+        /// Waiting during a limited time that the condtion change.
+        /// </summary>
+        /// <param name="secondsToWait">Time required before break the loop.</param>
+        /// <param name="whileCondition">If it's false, it will wait.</param>
+        public void Wait(int secondsToWait, bool whileCondition)
+        {
+            int _deltaT = 0;
+            int second = 0;
+            while (!whileCondition)
+            {
+                if (_deltaT != Cosmos.HAL.RTC.Second)
+                {
+                    second++;
+                    _deltaT = Cosmos.HAL.RTC.Second;
+                }
+
+                if (second >= secondsToWait)
+                {
+                    Apps.System.Debugger.debugger.Send("No response in " + secondsToWait + " secondes...");
+                    break;
+                }
+            }
+        }
+    }
+}
