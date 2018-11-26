dcd3a980b555e3f80df255f27a34c346279fa60d
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-11-27 00:05:08 +0100

    Ping restructuration : using timer & sending ping with dns

--- Aura Operating System/Aura OS/System/Network/IPV4/ICMP/Ping.cs
+++ Aura Operating System/Aura OS/System/Network/IPV4/ICMP/Ping.cs
@@ -7,6 +7,8 @@ namespace Aura_OS.System.Network.IPV4.ICMP
 {
     class Ping
     {
+        static Utils.Timer timer = new Utils.Timer();
+
         public static void Send(Address address)
         {
             string strAddress = address.ToString();
@@ -27,125 +29,110 @@ namespace Aura_OS.System.Network.IPV4.ICMP
                 }
             }
 
-            //Is it an IPv4 address ? or DNS ?
-            if (System.Utils.Misc.IsIpv4Address(items))
+            string IPdest = "";
+
+            int PacketSent = 0;
+            int PacketReceived = 0;
+            int PacketLost = 0;
+
+            int PercentLoss = 0;
+
+            try
             {
-                string IPdest = "";
+                Address destination = new Address((byte)(Int32.Parse(items[0])), (byte)(Int32.Parse(items[1])), (byte)(Int32.Parse(items[2])), (byte)(Int32.Parse(items[3])));
+                Address source = Config.FindNetwork(destination);
 
-                int PacketSent = 0;
-                int PacketReceived = 0;
-                int PacketLost = 0;
+                IPdest = destination.ToString();
 
-                int PercentLoss = 0;
-
-                try
-                {
-                    Address destination = new Address((byte)(Int32.Parse(items[0])), (byte)(Int32.Parse(items[1])), (byte)(Int32.Parse(items[2])), (byte)(Int32.Parse(items[3])));
-                    Address source = Config.FindNetwork(destination);
-
-                    IPdest = destination.ToString();
-
-                    int _deltaT = 0;
-                    int second;
-
-                    Console.WriteLine("Sending ping to " + destination.ToString());
-
-                    for (int i = 0; i < 4; i++)
-                    {
-                        second = 0;
-
-                        try
-                        {
-                            ICMPEchoRequest request = new ICMPEchoRequest(source, destination, 0x0001, 0x50);
-                            OutgoingBuffer.AddPacket(request);
-                            NetworkStack.Update();
-                        }
-                        catch (Exception ex)
-                        {
-                            CustomConsole.WriteLineError(ex.ToString());
-                        }
-
-                        PacketSent++;
-
-                        while (true)
-                        {
-
-                            if (ICMPPacket.recvd_reply != null)
-                            {
-                                Console.WriteLine("Reply received from " + ICMPPacket.recvd_reply.SourceIP.ToString());
-                                PacketReceived++;
-                                ICMPPacket.recvd_reply = null;
-
-                                break;
-                            }
-
-                            if (source == destination) //If we're pinging our interface => Ping ok
-                            {
-                                Console.WriteLine("Reply received from " + ICMPPacket.recvd_reply.SourceIP.ToString());
-                                ICMPPacket.recvd_reply = null;
-
-                                break;
-
-                            }
-                            else if (destination.IsLoopbackAddress()) //Loopback address => Ping ok
-                            {
-                                Console.WriteLine("Reply received from " + ICMPPacket.recvd_reply.SourceIP.ToString());
-                                ICMPPacket.recvd_reply = null;
-
-                                break;
-                            }
-
-                            if (second >= 5)
-                            {
-                                Console.WriteLine("Destination host unreachable.");
-                                PacketLost++;
-                                break;
-                            }
-
-                            if (_deltaT != Cosmos.HAL.RTC.Second)
-                            {
-                                second++;
-                                _deltaT = Cosmos.HAL.RTC.Second;
-                            }
-                        }
-                    }
-                }
-                catch
-                {
-                    Text.Display("notcorrectaddress");
-                }
-                finally
-                {
-                    PercentLoss = (100 / PacketSent) * PacketLost;
-
-                    Console.WriteLine();
-                    Console.WriteLine("Ping statistics for " + IPdest + ":");
-                    Console.WriteLine("    Packets: Sent = " + PacketSent + ", Received = " + PacketReceived + ", Lost = " + PacketLost + " (" + PercentLoss + "% loss)");
-                }
-            }
-            else
-            {
-                System.Network.IPV4.UDP.DNS.DNSClient DNSRequest = new System.Network.IPV4.UDP.DNS.DNSClient(53);
-                DNSRequest.Ask(strAddress);
                 int _deltaT = 0;
-                int second = 0;
-                while (!DNSRequest.ReceivedResponse)
+                int second;
+
+                Console.WriteLine("Sending ping to " + destination.ToString());
+
+                for (int i = 0; i < 4; i++)
                 {
-                    if (_deltaT != Cosmos.HAL.RTC.Second)
+                    second = 0;
+
+                    try
                     {
-                        second++;
-                        _deltaT = Cosmos.HAL.RTC.Second;
+                        ICMPEchoRequest request = new ICMPEchoRequest(source, destination, 0x0001, 0x50);
+                        OutgoingBuffer.AddPacket(request);
+                        NetworkStack.Update();
+                    }
+                    catch (Exception ex)
+                    {
+                        CustomConsole.WriteLineError(ex.ToString());
                     }
 
-                    if (second >= 4)
+                    PacketSent++;
+
+                    while (true)
                     {
-                        Apps.System.Debugger.debugger.Send("No response in 4 secondes...");
-                        break;
+
+                        if (ICMPPacket.recvd_reply != null)
+                        {
+                            Console.WriteLine("Reply received from " + ICMPPacket.recvd_reply.SourceIP.ToString());
+                            PacketReceived++;
+                            ICMPPacket.recvd_reply = null;
+
+                            break;
+                        }
+
+                        if (source == destination) //If we're pinging our interface => Ping ok
+                        {
+                            Console.WriteLine("Reply received from " + ICMPPacket.recvd_reply.SourceIP.ToString());
+                            ICMPPacket.recvd_reply = null;
+
+                            break;
+
+                        }
+                        else if (destination.IsLoopbackAddress()) //Loopback address => Ping ok
+                        {
+                            Console.WriteLine("Reply received from " + ICMPPacket.recvd_reply.SourceIP.ToString());
+                            ICMPPacket.recvd_reply = null;
+
+                            break;
+                        }
+
+                        if (second >= 5)
+                        {
+                            Console.WriteLine("Destination host unreachable.");
+                            PacketLost++;
+                            break;
+                        }
+
+                        if (_deltaT != Cosmos.HAL.RTC.Second)
+                        {
+                            second++;
+                            _deltaT = Cosmos.HAL.RTC.Second;
+                        }
                     }
                 }
-                DNSRequest.Close();
-                Send(DNSRequest.address);
             }
+            catch
+            {
+                Text.Display("notcorrectaddress");
+            }
+            finally
+            {
+                PercentLoss = (100 / PacketSent) * PacketLost;
+
+                Console.WriteLine();
+                Console.WriteLine("Ping statistics for " + IPdest + ":");
+                Console.WriteLine("    Packets: Sent = " + PacketSent + ", Received = " + PacketReceived + ", Lost = " + PacketLost + " (" + PercentLoss + "% loss)");
+            }
+
+        }
+
+        public static void Send(string Name)
+        {
+            UDP.DNS.DNSClient DNSRequest = new UDP.DNS.DNSClient(53);
+            DNSRequest.Ask(Name);
+
+            timer.Wait(4, DNSRequest.ReceivedResponse);
+
+            DNSRequest.Close();
+            Send(DNSRequest.address);
         }
     }
 }
