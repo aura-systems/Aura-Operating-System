81c15693d107d228bf08207d82035345263f5ef7
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-12-05 23:33:14 +0100

    Update Timer.cs

--- Aura Operating System/Aura OS/System/Utils/Timer.cs
+++ Aura Operating System/Aura OS/System/Utils/Timer.cs
@@ -6,6 +6,19 @@ namespace Aura_OS.System.Utils
 {
     class Timer
     {
+        public enum State
+        {
+            Waiting,
+            None,
+            Finished
+        }
+
+        public State Status
+        {
+            get;
+            set;
+        }
+
         /// <summary>
         /// Waiting during a limited time that the condtion change.
         /// </summary>
@@ -28,6 +41,27 @@ namespace Aura_OS.System.Utils
                     Apps.System.Debugger.debugger.Send("No response in " + secondsToWait + " secondes...");
                     break;
                 }
+            }
+        }
+
+        public void BreakWait(int secondsToWait)
+        {
+            int _deltaT = 0;
+            int second = 0;
+
+            if (_deltaT != Cosmos.HAL.RTC.Second)
+            {
+                second++;
+                _deltaT = Cosmos.HAL.RTC.Second;
+            }
+
+            if (second >= secondsToWait)
+            {
+                Status = State.Finished;
+            }
+            else
+            {
+                Status = State.Waiting;
             }
         }
     }
