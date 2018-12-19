3b6121cdbf04c11c4b76ff8e692e2dd4607e585b
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-12-19 23:07:46 +0100

    Update Timer

--- Aura Operating System/Aura OS/System/Utils/Timer.cs
+++ Aura Operating System/Aura OS/System/Utils/Timer.cs
@@ -6,25 +6,15 @@ namespace Aura_OS.System.Utils
 {
     class Timer
     {
-        public enum State
-        {
-            Waiting,
-            None,
-            Finished
-        }
-
-        public State Status
-        {
-            get;
-            set;
-        }
+        private int seconds;
+        private int max_seconds;
 
         /// <summary>
         /// Waiting during a limited time that the condtion change.
         /// </summary>
         /// <param name="secondsToWait">Time required before break the loop.</param>
         /// <param name="whileCondition">If it's false, it will wait.</param>
-        public void Wait(int secondsToWait, bool whileCondition)
+        public static void Wait(int secondsToWait, bool whileCondition)
         {
             int _deltaT = 0;
             int second = 0;
@@ -44,25 +34,24 @@ namespace Aura_OS.System.Utils
             }
         }
 
-        public void BreakWait(int secondsToWait)
+        public Timer(int secondsToWait)
+        {
+            seconds = 0;
+            max_seconds = secondsToWait;
+        }
+
+        public int IncrementSecond()
         {
             int _deltaT = 0;
-            int second = 0;
 
             if (_deltaT != Cosmos.HAL.RTC.Second)
             {
-                second++;
+                Console.WriteLine("status : second : " + seconds);
+                seconds++;
                 _deltaT = Cosmos.HAL.RTC.Second;
             }
 
-            if (second >= secondsToWait)
-            {
-                Status = State.Finished;
-            }
-            else
-            {
-                Status = State.Waiting;
-            }
+            return seconds;
         }
     }
 }
