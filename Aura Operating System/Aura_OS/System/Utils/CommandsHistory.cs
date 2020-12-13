03e1b8c7be8fc87a11f7a9e761fa916188eeef28
valentinbreiz <valentinbreiz@gmail.com>
2020-12-13 21:11:11 +0100

    Change ClearCurrentConsoleLine

--- Aura Operating System/Aura_OS/System/Utils/CommandsHistory.cs
+++ Aura Operating System/Aura_OS/System/Utils/CommandsHistory.cs
@@ -20,10 +20,10 @@ namespace Aura_OS.System.Utils
 
         public static void ClearCurrentConsoleLine()
         {
-            int currentLineCursor = Console.CursorTop - 1;
-            Console.SetCursorPosition(0, Console.CursorTop);
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, currentLineCursor);
             Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.SetCursorPosition(0, currentLineCursor);
         }
 
     }
