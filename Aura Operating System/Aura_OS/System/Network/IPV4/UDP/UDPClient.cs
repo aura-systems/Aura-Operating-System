f437ef5fb39a32705d8e6f3ce4df2e62fabc4aae
valentinbreiz <valentinbreiz@gmail.com>
2020-12-17 16:29:15 +0100

    Better Udp command and Receive command

--- Aura Operating System/Aura_OS/System/Network/IPV4/UDP/UDPClient.cs
+++ Aura Operating System/Aura_OS/System/Network/IPV4/UDP/UDPClient.cs
@@ -107,7 +107,7 @@ namespace Aura_OS.System.Network.IPV4.UDP
             }
         }
 
-        public byte[] Receive(ref IPV4.EndPoint source)
+        public byte[] NonBlockingReceive(ref IPV4.EndPoint source)
         {
             if (this.rxBuffer.Count < 1)
             {
@@ -121,13 +121,21 @@ namespace Aura_OS.System.Network.IPV4.UDP
             return packet.data;
         }
 
+        public byte[] Receive(ref IPV4.EndPoint source)
+        {
+            while (this.rxBuffer.Count < 1);
+
+            DataGram packet = rxBuffer.Dequeue();
+            source.address = packet.source.address;
+            source.port = packet.source.port;
+
+            return packet.data;
+        }
+
         internal void receiveData(IPV4.UDP.UDPPacket packet)
         {
             byte[] data = packet.UDP_Data;
             IPV4.EndPoint source = new IPV4.EndPoint(packet.SourceIP, packet.SourcePort);
-
-            Console.WriteLine("\nReceived UDP Packet (" + data.Length + "bytes) from " + source.ToString());
-            Console.WriteLine("Content: " + Encoding.ASCII.GetString(data));
 
             this.rxBuffer.Enqueue(new DataGram(data, source));
         }
