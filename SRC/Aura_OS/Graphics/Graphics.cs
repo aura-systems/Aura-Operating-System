4be1583506406a4d7d197bbe7dd5887896f7b07c
Valentin Charbonnier <valentinbreiz@gmail.com>
2023-06-19 14:49:07 +0200

    Remove useless method

--- SRC/Aura_OS/Graphics/Graphics.cs
+++ SRC/Aura_OS/Graphics/Graphics.cs
@@ -17,19 +17,6 @@ namespace Aura_OS.System.Graphics
 {
     public class GUI
     {
-        public void DrawFilledRectangle(Color pen, int x_start, int y_start, int width, int height)
-        {
-            if (height == -1)
-            {
-                height = width;
-            }
-
-            for (int y = y_start; y < y_start + height; y++)
-            {
-                Kernel.canvas.DrawLine(pen, x_start, y, x_start + width + 1, y);
-            }
-        }
-
         public void WriteByte(char ch, int mX, int mY, Color color)
         {
             Kernel.canvas.DrawChar((char)ch, Kernel.font, color, mX, mY);
@@ -39,7 +26,7 @@ namespace Aura_OS.System.Graphics
         {
             if (Kernel.console.CursorVisible)
             {
-                DrawFilledRectangle(Kernel.console.ForegroundColor, (int)Kernel.console.x + mX * Kernel.font.Width,
+                Kernel.canvas.DrawFilledRectangle(Kernel.console.ForegroundColor, (int)Kernel.console.x + mX * Kernel.font.Width,
                     (int)Kernel.console.y + mY * Kernel.font.Height + Kernel.font.Height, 8, 4);
             }
         }
