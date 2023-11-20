c52d8a080f8228f0b7b2d19007b00ac3529f04c2
Alexy DA CRUZ <dacruzalexy@gmail.com>
2023-11-20 16:24:56 +0100

    Fix domain name validation and add IP address validation and command line parsing

--- SRC/Aura_OS/System/Utils/Misc.cs
+++ SRC/Aura_OS/System/Utils/Misc.cs
@@ -36,6 +36,7 @@ namespace Aura_OS.System.Utils
         /// </summary>
         /// <param name="domainName">Valid or not valid domain name</param>
         /// <returns>Boolean depending of the domain name param</returns>
+        // https://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
         public static bool isValidDomainName(string domainName)
         {
             if (domainName.Length < 1)
@@ -49,18 +50,28 @@ namespace Aura_OS.System.Utils
             }
 
             string[] labels = domainName.Split('.');
-            if (labels.length > 63)
+            if (labels.Length > 63)
             {
                 return false;
             }
 
-            // to implement
-            // string pattern = @"^(?:(?=[a-z0-9-]{1,63}\.)[a-z0-9]+(?:-[a-z0-9]+)*\.)+[a-z]{2,}$";
-            // https://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
-            
+            string pattern = @"^(?:(?=[a-z0-9-]{1,63}\.)[a-z0-9]+(?:-[a-z0-9]+)*\.)+[a-z]{2,}$";
+            Regex regex = new Regex(pattern);
+
+            if (regex.IsMatch(domainName)) {
+                return true;
+            } else {
+                return false
+            }
+
             return true;
         }
 
+        /// <summary>
+        /// Check if the IP address is valid based on information from RFC 791
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
         public static bool IsIPv4Address(string ip)
         {
             string[] items = ip.Split('.');
@@ -74,6 +85,12 @@ namespace Aura_OS.System.Utils
             }
         }
 
+        
+        /// <summary>
+        /// Parse a command line
+        /// </summary>
+        /// <param name="cmdLine"></param>
+        /// <returns></returns>
         //https://stackoverflow.com/questions/59638467/parsing-command-line-args-with-quotes
         public static List<string> ParseCommandLine(string cmdLine)
         {
